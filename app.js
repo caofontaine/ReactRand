47f9ee2401ee77da44e373e36b701087cb202561
Ben Cao <benjamin.cao88@gmail.com>
2017-06-10 16:14:19 -0400

    CSS/Formula Fix
    Added CSS and fixed randomize formula.

--- app.js
+++ app.js
@@ -11,17 +11,6 @@ class RandomNum extends React.Component {
     };
   }
   
-  onRandomizeClick(evt) {
-    if(this.state.lowBound >= this.state.highBound) {
-      alert("Bounds are incorrect.");
-    }
-    else {
-      this.setState({
-        randomNum: Math.floor((Math.random() * this.state.highBound + 1) + this.state.lowBound)
-      });
-    }
-  }
-  
   handleLowBound(e) {
     this.setState({
       lowBound: e.target.value
@@ -34,20 +23,32 @@ class RandomNum extends React.Component {
     });
   }
   
+  onRandomizeClick(evt) {
+    var low = parseInt(this.state.lowBound), high = parseInt(this.state.highBound);
+    if(low >= high) {
+      alert("Bounds are incorrect.");
+    }
+    else {
+      this.setState({
+        randomNum: Math.floor((Math.random() * ((high-low) + 1)) + low)
+      });
+    }
+  }
+  
   render() {  
     return (
       <div>
         <h2>Low Bound</h2>
-        <input type="number" className="lowbound" onChange={this.handleLowBound.bind(this)} value={this.state.lowBound}/>
+        <input type="text" className="lowbound" onChange={this.handleLowBound.bind(this)} value={this.state.lowBound}/>
         
         <h2>High Bound</h2>
-        <input type="number" className="highbound" onChange={this.handleHighBound.bind(this)} value={this.state.highBound}/>
+        <input type="text" className="highbound" onChange={this.handleHighBound.bind(this)} value={this.state.highBound}/>
         
         <p>{this.state.randomNum}</p>
         <button onClick={this.onRandomizeClick.bind(this)}>Generate</button>
-        //Class methods are not bound by default. So bind() is necessary to use 'this' properly.
       </div>
     );
+        //Class methods are not bound by default. So bind() is necessary to use 'this' properly.
   }
 }
 
